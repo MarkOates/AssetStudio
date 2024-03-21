4365b6a33b406daf3d81422a715b57f84fa27f4a
Mark Oates <markoates0@gmail.com>
2024-03-21 18:17:54 -0400

    Include gid with sheet when exporting

--- scripts/remote_asset_sync.sh
+++ scripts/remote_asset_sync.sh
@@ -2,7 +2,7 @@
 
 #CSV_URL="https://docs.google.com/spreadsheets/d/1vedD3jUY7_w5pRCI3hUWNhPzehzoGecKdoAmlOqbAig/edit#gid=0"
 # CSV_URL="https://docs.google.com/spreadsheets/d/1vedD3jUY7_w5pRCI3hUWNhPzehzoGecKdoAmlOqbAig/export?exportFormat=csv"
-CSV_URL="https://docs.google.com/spreadsheets/d/1qWKh4-TxAhIQEa7LUg-ih9xsCWANSLRtvwzVyYH0HIM/export?exportFormat=csv"
+CSV_URL="https://docs.google.com/spreadsheets/d/1qWKh4-TxAhIQEa7LUg-ih9xsCWANSLRtvwzVyYH0HIM/export?exportFormat=csv&gid=646986983"
 # edit#gid=646986983"
 # CSV_URL="https://docs.google.com/spreadsheets/d/1qWKh4-TxAhIQEa7LUg-ih9xsCWANSLRtvwzVyYH0HIM/edit#gid=646986983"
 # curl -L "https://docs.google.com/spreadsheets/d/1By-EzGexU9DHZgtKFR6XwftfL13Su9Kff4KFcpRR7dg/export?exportFormat=csv" --output /Users/markoates/Repos/SurviveTheCity/bin/data/levels/universe.csv
